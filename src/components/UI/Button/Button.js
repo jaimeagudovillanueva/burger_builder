403c971492755796c8ef44f17a7cb9a5fc65029b
Jaime Agudo <jaime.agudo@externos.seap.minhap.es>
2020-04-20 15:55:11 +0200

    Adding forms with generic inputs an validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,10 @@ import React from 'react';
 import styles from './Button.module.css';
 
 const button = props => (
-    <button className={[styles['Button'], styles[props.btnType]].join(' ')} onClick={props.clicked}>
+    <button 
+        disabled={props.disabled}
+        className={[styles['Button'], styles[props.btnType]].join(' ')} 
+        onClick={props.clicked}>
         {props.children}
     </button>
  );
